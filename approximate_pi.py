63e721e6c45cbd391e81eae687b3d3561e41bd26
Sebastien <granie.sebastien7@gmail.com>
2020-10-26 20:14:32 +0100

    Premier resultat fonctionnel, à améliorer

--- approximate_pi.py
+++ approximate_pi.py
@@ -1,32 +1,45 @@
 #!/usr/bin/env python3
-
+'''
+pi
+'''
 import argparse
-import simulator
+import copy
+from cv2 import cv2
 import imageio
 import numpy as np
+import simulator
 
 
 def generate_ppm_file(taille, nb_points, decimale):
-    image = np.zeros((taille, taille, 3), dtype="uint8")
-    simulator.simulator(nb_points, image)
-    imageio.imwrite("image_pi.jpg", image)
-    
-
-
-
-
-
-
+    '''
+    Cette fonction génère des images
+    pour approximer pi de manière
+    graphique.
+    '''
+    image = np.full((taille, taille, 3), 255, dtype="uint8")
+    pi_global = 0
+    images = []
+    for i in range(0, 10):
+        pi_estime = simulator.simulator(int(nb_points/10), image)
+        images.append(copy.deepcopy(image))
+        pi_global += pi_estime
+        pi_fin = pi_global/(i+1)
+        partie_decimale = int((pi_fin-int(pi_fin))*(10**decimale))
+        nb_decimal = f".{decimale}f"
+        text = f"{format(pi_fin,nb_decimal)}"
+        (text_height, text_width), _ = cv2.getTextSize(text,\
+                                       cv2.FONT_HERSHEY_SIMPLEX, 2, 5)
+        cv2.putText(images[i],text, (int(taille/2-text_height/2),\
+                                     int(taille/2+text_width/2)),\
+                                     cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 5)
+        imageio.imwrite(f"out/img{i}_{int(pi_fin)}-{partie_decimale}.ppm", image)
+    imageio.mimsave('out/gif_seb.gif', images, duration=1)
 
 if __name__ == "__main__":
-    
     parser = argparse.ArgumentParser(description="generation image")
     parser.add_argument('taille_image', action='store', type=int)
     parser.add_argument('nombre_de_point', action='store', type=int)
     parser.add_argument('nombre_chiffre_virgule', action='store', type=int)
-
-
     arguments = parser.parse_args()
-
-    generation_image = generate_ppm_file(arguments.taille_image, arguments.nombre_de_point, arguments.nombre_chiffre_virgule)
-    # simulator = simulator(arguments.nombre_de_point, image)
+    generate_ppm_file(arguments.taille_image, arguments.nombre_de_point, \
+                      arguments.nombre_chiffre_virgule)
--- simulator.py
+++ simulator.py
@@ -1,44 +1,41 @@
-#!/usr/bin/env python3
-
-import random
-from math import sqrt
+#!/usr/bin/env python3.7
+'''
+pi
+'''
 import argparse
-import numpy as np
+from math import sqrt
+import random
 
-def simulator(n, image = None):
+def simulator(points, image = None):
     '''
-    Cette fonction retourne une 
+    Cette fonction retourne une
     approximation du nombre pi en générant
     des points appartenant ou non à un cercle.
     '''
-
     compteur = 0
     if image is not None:
         taille = image.shape[0] #return taille des x
         demi_taille = int(taille/2)
-    for _ in range(1, n):
-        x = random.uniform(-1, 1)
-        y = random.uniform(-1, 1)
+    for _ in range(1, points):
+        point_x = random.uniform(-1, 1)
+        point_y = random.uniform(-1, 1)
         # print(f"({x},{y})")
-        distance = x**2 + y**2
+        distance = point_x**2 + point_y**2
         distance = sqrt(float(distance))
         if float(distance) < 1:
             if image is not None:
-                image[int((x+1)*demi_taille)][int((y+1)*demi_taille)] = (0, 0, 255)
+                image[int((point_x+1)*demi_taille)]\
+                     [int((point_y+1)*demi_taille)] = (0, 0, 255)
             compteur +=1
         else:
             if image is not None:
-                image[int((x+1)*demi_taille)][int((y+1)*demi_taille)] = (238, 130, 238)
-
-    return (float(compteur/n)*4)
-
+                image[int((point_x+1)*demi_taille)]\
+                     [int((point_y+1)*demi_taille)] = (238, 130, 238)
+    return float(compteur/points)*4
 
 if __name__ == "__main__":
-    
     parser = argparse.ArgumentParser(description="Approximation de pi")
     parser.add_argument('nombre_de_points', action='store', type=int)
-
     arguments = parser.parse_args()
-
     pi = simulator(arguments.nombre_de_points)
     print(pi)
